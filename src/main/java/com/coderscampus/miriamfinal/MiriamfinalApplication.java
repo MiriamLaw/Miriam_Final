deda753547fe79495d5216dec39a5de2cb9eba55
MiriamLaw <mimsical999@yahoo.com>
2024-10-17 11:04:53 -0600

    Initial commit for new Spring project

---
+++ src/main/java/com/coderscampus/miriamfinal/MiriamfinalApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.miriamfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiriamfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiriamfinalApplication.class, args);
+	}
+
+}
