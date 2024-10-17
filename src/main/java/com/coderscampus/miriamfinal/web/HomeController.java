5972ab1d6d4fa82a54133e7ab4581cf60712b1cc
MiriamLaw <mimsical999@yahoo.com>
2024-10-17 09:19:56 -0600

    fixed package structure

--- src/main/java/com/coderscampus/miriamfinal/web/HomeController.java
+++ src/main/java/com/coderscampus/miriamfinal/web/HomeController.java
@@ -1,4 +1,4 @@
-package com.coderscampus.miriamfinal;
+package com.coderscampus.miriamfinal.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
