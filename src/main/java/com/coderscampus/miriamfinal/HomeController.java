2b5c7eaf4d23755183993a9604b2e00296c6029b
MiriamLaw <mimsical999@yahoo.com>
2024-10-17 09:08:40 -0600

    fixed repo and added homecontroller

---
+++ src/main/java/com/coderscampus/miriamfinal/HomeController.java
@@ -0,0 +1,19 @@
+package com.coderscampus.miriamfinal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, home!";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, secured!";
+    }
+}
---
+++ src/test/java/com/coderscampus/miriamfinal/MiriamfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.miriamfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiriamfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
